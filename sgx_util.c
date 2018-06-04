98c8370098a5b3f2a74bf7fe4173dac048945a7e
EugeneKuz <eugenekuznetsov@Hotmail.com>
2018-06-03 20:05:18 -0700

    Update sgx_util.c

--- sgx_util.c
+++ sgx_util.c
@@ -382,3 +382,13 @@ void sgx_etrack(struct sgx_encl *encl)
 		sgx_invalidate(encl, true);
 	}
 }
+
+void print_encl_stats(struct sgx_encl *encl) {
+
+  printk("\n\n\n******************** Enclave # %u ********************\nTotal Paged: %u \nTotal Loaded: %u \nTotal Evicted: %u \n*****************************************************\n\n\n", 
+	 encl->enclave_number,
+	 encl->epc_total_paged,
+	 encl->epc_total_loaded,
+	 encl->epc_total_evicted);
+         
+}
