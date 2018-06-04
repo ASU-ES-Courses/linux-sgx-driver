85fb7f7b1adfa04932d6afa4d14be50f4c4fc19b
EugeneKuz <eugenekuznetsov@Hotmail.com>
2018-06-03 19:34:52 -0700

    Update debug.c

--- debug.c
+++ debug.c
@@ -5,9 +5,9 @@
 void print_encl_stats(struct sgx_encl *encl) {
 
   printk("\n\n\n******************** Enclave # %u ********************\nTotal Paged: %u \nTotal Loaded: %u \nTotal Evicted: %u \n*****************************************************\n\n\n", 
-	       encl->enclave_number,
-         encl->epc_total_paged,
-	       encl->epc_total_loaded,
-	       encl->epc_total_evicted);
+	 encl->enclave_number,
+	 encl->epc_total_paged,
+	 encl->epc_total_loaded,
+	 encl->epc_total_evicted);
          
 }
