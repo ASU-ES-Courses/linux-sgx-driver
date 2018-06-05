0cb5492c5c061f68da6b4f8f905811f2557ac6b6
EugeneKuz <eugenekuznetsov@Hotmail.com>
2018-06-05 14:20:59 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ sudo cp isgx.ko "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"
 sudo sh -c "cat /etc/modules | grep -Fxq isgx || echo isgx >> /etc/modules"
 sudo /sbin/depmod
 sudo /sbin/modprobe isgx
+chmod +x uninstall.sh
