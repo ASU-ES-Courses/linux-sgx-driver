5e3ec676bf335181900598dae1619a10e0fbeb49
EugeneKuz <eugenekuznetsov@Hotmail.com>
2018-07-04 16:58:15 -0700

    Update sgx_util.c

--- sgx_util.c
+++ sgx_util.c
@@ -250,11 +250,9 @@ static struct sgx_encl_page *sgx_do_fault(struct vm_area_struct *vma,
 	int rc = 0;
 	
 	//**** CHANGES MADE HERE *******//
-	/*
 	if (!print_once)
-		pr_info("%s: Executing SGX page fault\n", __func__);
+		pr_info("\n%s: Executing SGX page fault\n", __func__);
 	print_once++;
-	*/
 
 	/* If process was forked, VMA is still there but vm_private_data is set
 	 * to NULL.
